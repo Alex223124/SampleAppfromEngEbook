f7475c6b40f075b45cef8a2207ced06d5e6e45e4
alex223124 <dadeveloper@hotmail.com>
2015-05-21 18:59:52 +0000

    Add user microposts

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,4 +1,4 @@
-	require 'test_helper'
+require 'test_helper'
 class UserTest < ActiveSupport::TestCase
 
   def setup
@@ -65,14 +65,21 @@ class UserTest < ActiveSupport::TestCase
   
 
 
-  test "authenticated? should return false for a user with nil digest" do
-      assert_not @user.authenticated?(:remember, '')
-  end
+    test "authenticated? should return false for a user with nil digest" do
+        assert_not @user.authenticated?(:remember, '')
+    end
+
+
+
+   test "associated microposts should be destroyed" do
+      @user.save
+      @user.microposts.create!(content: "Lorem ipsum")
+      assert_difference 'Micropost.count', -1 do
+        @user.destroy
+      end
+    end
+    
 end
-
-
-
-
 
 
 
