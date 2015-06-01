72976581346072b443f58452f90772b69b4cd3c4
alex223124 <dadeveloper@hotmail.com>
2015-06-01 10:57:04 +0000

    Add user following

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,16 @@
 class User < ActiveRecord::Base
   has_many :microposts, dependent: :destroy
+  has_many :active_relationships,  class_name:  "Relationship",
+                                   foreign_key: "follower_id",
+                                   dependent:   :destroy
+  has_many :passive_relationships, class_name:  "Relationship",
+                                   foreign_key: "followed_id",
+                                   dependent:   :destroy
+  has_many :following, through: :active_relationships,  source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
+
+
+
 
   attr_accessor :remember_token, :activation_token, :reset_token
   before_save   :downcase_email
@@ -23,10 +34,12 @@ class User < ActiveRecord::Base
                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
+	
 	# Returns a random token.
   def User.new_token
     SecureRandom.urlsafe_base64
   end
+	
 	# Remembers a user in the database for use in persistent sessions.
   def remember
     self.remember_token = User.new_token
@@ -69,15 +82,35 @@ class User < ActiveRecord::Base
     UserMailer.password_reset(self).deliver_now
   end
   
+  # Returns true if a password reset has expired.
   def password_reset_expired?
     reset_sent_at < 2.hours.ago
-  end
-	
-	# Defines a proto-feed.
-  # See "Following users" for the full implementation.
+  end	# Returns a user's status feed.
+  
+  # Returns a user's status feed.
   def feed
-    Micropost.where("user_id = ?", id)
+    following_ids = "SELECT followed_id FROM relationships
+                     WHERE  follower_id = :user_id"
+    Micropost.where("user_id IN (#{following_ids})
+                     OR user_id = :user_id", user_id: id)
   end
+
+
+  # Follows a user.
+  def follow(other_user)
+    active_relationships.create(followed_id: other_user.id)
+  end
+
+
+# Unfollows a user.
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+# Returns true if the current user is following the other user.
+  def following?(other_user)
+    following.include?(other_user)
+  end
+
   
   private
 	# Converts email to all lower-case.
